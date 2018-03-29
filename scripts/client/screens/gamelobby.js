67fb699a8c245d2b00681502e1a7bf2574616e27
Hailee Maxwell <hailee.maxwell@gmail.com>
2018-03-29 12:45:10 -0600

    setting up gamelobby html and css

--- scripts/client/screens/gamelobby.js
+++ scripts/client/screens/gamelobby.js
@@ -4,15 +4,44 @@ Game.screens['gamelobby'] = (function(menu) {
   function initialize() {
     menu.addScreen('gamelobby',
       `
-      <h1>Game Lobby</h1>
-      <ul class = "menu">
-        <li><button id = "id-gamelobby-back">Back</button></li>
-      </ul>
-    `);
+			<div id="lobby-container">
+			<div id="message-body">
+				<div id="read-messages">
+					<ul id="messages"></ul>
+				</div>
+				<div id="enter-messages">
+					<input type="text" id="msg" name="msg">
+					<button id="msg-enter-btn">Chat</button>
+				</div>
+			</div>
+			<div id="lobby-body">
+				<p id="lobby-announcements"></p>
+
+				<div id="exit-lobby">
+    		  <ul class = "menu">
+       			<li><button id = "id-gamelobby-back">Back</button></li>
+      		</ul>
+				</div>
+			</div>
+			</div>
+   	  `
+		);
 
     document.getElementById('id-gamelobby-back').addEventListener(
       'click',
       function() { menu.showScreen('main-menu'); });
+		
+		document.getElementById('msg-enter-btn').addEventListener(
+			'click',
+			function() {
+				let message = document.getElementById('msg').value;
+				document.getElementById('msg').value = '';
+				$("#messages").append($("<li>").text(message));
+
+				let scroller = document.getElementById('read-messages');
+				scroller.scrollTop = scroller.scrollHeight;
+			}
+		);
   }
 
   function run() {
