aa85b5be2b350fd497ab8292b3be613213227f46
Sailanarmo <rlramirez12@gmail.com>
2018-04-21 18:34:05 -0600

    Ples make look purdy

--- scripts/client/screens/help.js
+++ scripts/client/screens/help.js
@@ -1,52 +1,66 @@
 Game.screens['help'] = (function(menu, keyBindings) {
   'use strict';
 
-  let forward = keyBindings.getBinding(keyBindings.keys.forward.key);
-  let back = keyBindings.getBinding(keyBindings.keys.back.key);
-  let left = keyBindings.getBinding(keyBindings.keys.left.key);
-  let right = keyBindings.getBinding(keyBindings.keys.right.key);
-  let fire = keyBindings.getBinding(keyBindings.keys.fire.key);
+  let forward,
+  back,
+  left,
+  right,
+  fire;
   
   
   function initialize() {
     menu.addScreen('help',
       `
       <h1>Help</h1>
-      <p>This is some help on how to play the game</p>
-	  <table style = "width:50%">
+            <ul id = "ree"></ul>
+      <ul class = "menu">
+<p>A Battle Royal! Be the last person to survive to win the game! When you spawn in the world, make sure you pick up a weapon! Grab some ammo, shields, and survive!</p>
+	  <table style = "width:40%" class = "inlineTable">
 	    <caption>Controls</caption>
 		<tr>
 		  <th>Move Up</th>
-		  <th id = "forward">a</th>
+		  <th id = "forward-input">a</th>
 		</tr>
 		<tr>
 		  <th>Move Left</th>
-		  <th id = "left">a</th>
+		  <th id = "left-input">a</th>
 		</tr>
 		<tr>
 		  <th>Move Back</th>
-		  <th id = "back">a</th>
+		  <th id = "back-input">a</th>
 		</tr>
 		<tr>
 		  <th>Move Right</th>
-		  <th id = "right">a</th>
+		  <th id = "right-input">a</th>
 		</tr>
         <tr>
 		  <th>Shoot Projectiles</th>
-		  <th id = "fire">a</th>
+		  <th id = "fire-input">a</th>
 		</tr>
 	  </table>
-      <ul id = "ree"></ul>
-      <ul class = "menu">
+	  <table style = "width:40%" class = "inlineTable">
+	    <caption>Items</caption>
+		<tr>
+		  <th>Gun</th>
+		  <th>Weapon Pickup</th>
+		</tr>
+		<tr>
+		  <th>Energy</th>
+		  <th>Use to get ammo</th>
+		</tr>
+		<tr>
+		  <th>Space Time Refractor</th>
+		  <th>Use to make bullets travel faster</th>
+		</tr>
+		<tr>
+		  <th>Health</th>
+		  <th>Use to repair your ship</th>
+	  </table>
         <li><button id = "id-help-back">Back</button></li>
+
       </ul>
     `);
 
-    //document.getElementById('forward').innerHTML = forward;
-    //document.getElementById('left').innerHTML = left;
-    //document.getElementById('back').innerHTML = back;
-    //document.getElementById('right').innerHTML = right;
-    //document.getElementById('fire').innerHTML = fire;
     
 	document.getElementById('id-help-back').addEventListener(
       'click',
@@ -61,16 +75,12 @@ Game.screens['help'] = (function(menu, keyBindings) {
     back = keyBindings.getBinding(keyBindings.keys.back.key);
     right = keyBindings.getBinding(keyBindings.keys.right.key);
     fire = keyBindings.getBinding(keyBindings.keys.fire.key);
-  console.log(forward);
-  console.log(back);
-  console.log(left);
-  console.log(right);
-  console.log(fire);
-	document.getElementById('forward').innerHTML = forward;
-	document.getElementById('left').innerHTML = left;
-	document.getElementById('back').innerHTML = back;
-	document.getElementById('right').innerHTML = right;
-	document.getElementById('fire').innerHTML = fire;
+	
+	document.getElementById('forward-input').innerHTML = forward;
+	document.getElementById('left-input').innerHTML = left;
+	document.getElementById('back-input').innerHTML = back;
+	document.getElementById('right-input').innerHTML = right;
+	document.getElementById('fire-input').innerHTML = fire;
   }
 
   return {
