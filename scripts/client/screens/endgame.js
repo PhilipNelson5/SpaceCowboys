b199ef6ee214db5d976d63277911446c2ea6e1df
Ammon <ammonhepworth@outlook.com>
2018-04-23 20:35:32 -0600

    commit the actual end screen lol

---
+++ scripts/client/screens/endgame.js
@@ -0,0 +1,49 @@
+Game.screens['endgame'] = (function(menu, socket) {
+  'use strict';
+
+
+
+
+  function initialize() {
+    menu.addScreen('endgame',
+      `
+      <div id="main-container">
+        <table id="endgame-stat-table">
+          <tr>
+            <td>Place</td>
+            <td>Player</td>
+            <td>Kills</td>
+            <td>Damage Dealt</td>
+            <td>Accuracy</td>
+          </tr>
+        </table>
+      </div>
+      `
+    );
+
+    socket.on(NetworkIds.GET_GAME_STATS, function(playerStats) {
+      let table = document.getElementById('endgame-stat-table');
+      playerStats.sort( (obj) => obj.place );
+      console.log(JSON.stringify(playerStats));
+      for (let id in playerStats) {
+        let row = table.insertRow(-1);
+        row.insertCell(-1).innerHTML = playerStats[id].place;
+        row.insertCell(-1).innerHTML = playerStats[id].username;
+        row.insertCell(-1).innerHTML = playerStats[id].kills;
+        row.insertCell(-1).innerHTML = playerStats[id].accuracy;
+        row.insertCell(-1).innerHTML = playerStats[id].damage;
+      }
+    });
+  }
+
+
+  function run() {
+    console.log('game has ended, displaying end screen');
+  }
+
+  return {
+    initialize,
+    run
+  };
+
+}(Game.menu, Game.network.socket));
